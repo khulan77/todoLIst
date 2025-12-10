63a454ecf10b5c368af3e6d66138f80bc2cb7d1e
m2 <dhulan174@gmail.com>
2025-12-10 19:01:37 +0800

    local changes

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -3,8 +3,5 @@ const config = {
     "@tailwindcss/postcss": {},
   },
 };
-<<<<<<< HEAD
 
-=======
->>>>>>> b3b955d494e77c583eaaed04ce7637c858d9ab77
 export default config;
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,11 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-<<<<<<< HEAD
       <body className="antialiased">
-=======
-      <body>
->>>>>>> b3b955d494e77c583eaaed04ce7637c858d9ab77
         <Main />
         <NextScript />
       </body>
